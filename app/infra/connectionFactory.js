cda6c49bdcb5c6d7faa80fcd373b5bb0f5a76177
Augusto Inacio <augustoinacio@gmail.com>
2017-12-10 17:03:57 -0200

    Configura db heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -16,6 +16,16 @@ function createDBConnection(){
             database: 'casaCodigoNodejs_test'
         });
     }
+    if(process.env.NODE_ENV === 'test'){
+        var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)?(.*)\?reconnect=true/);
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[5]
+        });
+    }
 }
 
 module.exports = function(){
